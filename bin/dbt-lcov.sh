7a0b84fa93e9043d309c686f6d6b5572d4ba06c2
Eric Flumerfelt <eflumerf@fnal.gov>
2021-06-29 16:19:56 -0500

    Add execution of test apps/scripts

--- bin/dbt-lcov.sh
+++ bin/dbt-lcov.sh
@@ -32,8 +32,53 @@ lcov -c -i -d . -o dunedaq.base
 # RUN THE TESTS
 dbt-build.sh --unittest
 
+       
+  COL_YELLOW="\e[33m"
+  COL_RESET="\e[0m"
+  COL_RED="\e[31m"
+  echo
+  echo
+  echo 
+
+  package_list=$( find -L build -mindepth 1 -maxdepth 1 -type d -not -name CMakeFiles )
+
+  for pkgname in $package_list ; do
+
+    testdirs=$( find -L $pkgname/test -type d \( -name "apps" -o -name "scripts" \) -not -regex ".*CMakeFiles.*" )
+
+    if [[ -z $testdirs ]]; then
+      echo
+      echo -e "${COL_RED}No test applications or scripts for $pkgname${COL_RESET}"
+      echo
+      continue
+    fi
+
+    num_tests=0
+
+    for testdir in $testdirs; do
+      echo
+      echo
+      echo "RUNNING TESTS IN $testdir"
+      echo "======================================================================"
+      for atest in $testdir/* ; do
+        if [[ -x $atest ]]; then
+          echo
+          echo -e "${COL_YELLOW}Start of test \"$atest\"${COL_RESET}"
+          $atest
+          echo -e "${COL_YELLOW}End of test \"$atest\"${COL_RESET}"
+          num_tests=$((num_tests + 1))
+        fi
+      done
+
+    done
+
+    echo 
+    echo -e "${COL_YELLOW}Testing complete for package \"$pkgname\". Ran $num_tests tests.${COL_RESET}"
+  done
+     
+
 lcov -d . --capture --output-file dunedaq.info
 lcov -a dunedaq.base -a dunedaq.info --output-file dunedaq.total
 lcov --remove dunedaq.total '*/products/*' '/usr/include/*' '/cvmfs/*' "$DBT_AREA_ROOT/build/*" --output-file dunedaq.info.cleaned
-genhtml -o coverage dunedaq.info.cleaned
+genhtml --demangle-cpp -o coverage dunedaq.info.cleaned
 #genhtml -o coverage dunedaq.info
