45a1be80b505202535c279636a470f4f1e6f4a0e
John Freeman <jcfree@mu2edaq13.fnal.gov>
2021-07-13 21:26:28 -0500

    JCF: get dbt-create.sh to point you to the modern documentation

--- bin/dbt-create.sh
+++ bin/dbt-create.sh
@@ -195,7 +195,7 @@ echo "Total time to run "$( basename $0)": "$(( endtime_s - starttime_s ))" seco
 echo "Start time: $starttime_d"
 echo "End time:   $endtime_d"
 echo
-echo "See https://github.com/DUNE-DAQ/appfwk/wiki/Compiling-and-running for build instructions"
+echo "See https://dune-daq-sw.readthedocs.io/en/latest/packages/daq-buildtools/index.html for build instructions"
 echo
 echo "Script completed successfully"
 echo
