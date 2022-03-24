f0872d741609c64569bf26d0ac327d2b1773729f
John Freeman <jcfree@mu2edaq13.fnal.gov>
2022-03-24 13:36:26 -0500

    JCF: Issue #161: update the how-to-get-Python-3 printout to accommodate Spack rather than UPS

--- bin/dbt-create.py
+++ bin/dbt-create.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python
 import sys
 if sys.version_info[0] < 3:
-    raise Exception("""Python 3 is required.
-On systems with cvmfs, you can obtain python 3 by:
-$ source /cvmfs/dunedaq.opensciencegrid.org/products/setup; setup python""")
+    if "--spack" not in sys.argv:
+        raise Exception("""Python 3 is required.
+    On systems with cvmfs, you can obtain python 3 by:
+    $ source /cvmfs/dunedaq.opensciencegrid.org/products/setup; setup python""")
+    else:
+        raise Exception("""Python 3 is required. On systems with cvmfs, you can obtain Python 3 by executing the following:
+
+        source /cvmfs/dunedaq-development.opensciencegrid.org/sandbox/spack/spack/share/spack/setup-env.sh 
+        spack load python@3.8.3%gcc@8.2.0""")
 
 import os
 DBT_ROOT=os.environ["DBT_ROOT"]
