25c389c0ab5463c9549a2df8233c88407f241616
Pengfei Ding <dingpf@fnal.gov>
2022-08-31 03:17:11 -0500

    dbt change to support gcc-12 build

--- scripts/dbt_setup_constants.py
+++ scripts/dbt_setup_constants.py
@@ -1,8 +1,8 @@
 DBT_VENV="dbt-pyvenv"
 DBT_AREA_FILE="dbt-workarea-constants.sh"
 
-PROD_BASEPATH="/cvmfs/dunedaq.opensciencegrid.org/spack-releases"
-NIGHTLY_BASEPATH="/cvmfs/dunedaq-development.opensciencegrid.org/spack-nightly"
+PROD_BASEPATH="/cvmfs/dunedaq.opensciencegrid.org/spack/releases"
+NIGHTLY_BASEPATH="/cvmfs/dunedaq-development.opensciencegrid.org/nightly"
 CANDIDATE_RELEASE_BASEPATH="/cvmfs/dunedaq-development.opensciencegrid.org/candidate-releases"
 
 
