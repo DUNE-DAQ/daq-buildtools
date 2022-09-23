2d1b6f286a2df5bfad082d8697f511ae54e5563b
Pengfei Ding <dingpf@fnal.gov>
2022-09-23 18:29:33 -0500

    use different path for candidate releases for gcc-12

--- scripts/dbt-setup-constants.sh
+++ scripts/dbt-setup-constants.sh
@@ -3,4 +3,4 @@ DBT_AREA_FILE="dbt-workarea-constants.sh"
 
 PROD_BASEPATH="/cvmfs/dunedaq.opensciencegrid.org/releases"
 NIGHTLY_BASEPATH="/cvmfs/dunedaq-development.opensciencegrid.org/nightly"
-CANDIDATE_RELEASE_BASEPATH="/cvmfs/dunedaq-development.opensciencegrid.org/candidate-releases"
+CANDIDATE_RELEASE_BASEPATH="/cvmfs/dunedaq-development.opensciencegrid.org/candidates"
