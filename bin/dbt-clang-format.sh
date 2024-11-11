b10ad7aa83cb506c2f987aea7f2e2885bf5303f7
Andrew Mogan <amogan@fnal.gov>
2024-11-11 13:55:07 -0600

    Remove debug echo statements

--- bin/dbt-clang-format.sh
+++ bin/dbt-clang-format.sh
@@ -142,13 +142,10 @@ function format_files() {
     for orig_file in $files_to_format ; do
 
     this_package_name=$(echo "$orig_file" | cut -d '/' -f 2)
-    echo "This package is ${this_package_name}"
     if [[ "$this_package_name" != "$previous_package_name" ]]; then
         markdown_content+="## $this_package_name \n"
         markdown_content+="| Test | Status| \n| --- | --- |\n"
         previous_package_name=$this_package_name
-        echo "Previous package is now ${previous_package_name}"
-        echo "Current markdown content: ${markdown_content}"
     fi
 
 	echo "Processing ${orig_file}..."
