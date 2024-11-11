fd0adbef57ddd7d2e2ebe41be3e053c8399e4b3a
Andrew Mogan <amogan@fnal.gov>
2024-11-11 13:37:51 -0600

    Sort files to format by package name

--- bin/dbt-clang-format.sh
+++ bin/dbt-clang-format.sh
@@ -188,7 +188,11 @@ files_to_format=""
 extensions="*.hpp *.cpp *.cxx *.hxx"
 
 if [[ -d $filename ]]; then
-    files_to_format=$( for extension in $extensions ; do find $filename -name $extension; done )
+    files_to_format=$( 
+        for extension in $extensions ; do 
+            find $filename -name $extension; 
+        done | sort -t '/' -k 2
+    )
 elif [[ -f $filename ]]; then
     extension=$( echo $filename | sed -r 's/.*\.([^.]+)$/\1/' )
 
