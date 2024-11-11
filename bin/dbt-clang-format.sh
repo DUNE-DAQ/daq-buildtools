738398a80e8e9ac2a4406b84ef0343265afd77db
Andrew Mogan <amogan@fnal.gov>
2024-11-11 13:23:42 -0600

    Fix package name parsing

--- bin/dbt-clang-format.sh
+++ bin/dbt-clang-format.sh
@@ -141,7 +141,7 @@ function format_files() {
 
     for orig_file in $files_to_format ; do
 
-    this_package_name=$(echo "$orig_file" | cut -d '/' -f 1)
+    this_package_name=$(echo "$orig_file" | cut -d '/' -f 2)
     echo "This package is ${this_package_name}"
     if [[ "$this_package_name" != "$previous_package_name" ]]; then
         markdown_content+="## $this_package_name \n"
